aef781dc74dce66a53a1d0cab07b3217ad8c89db
Andrew Baird <andrew.baird@currance.com>
2021-05-06 14:01:30 -0700

    Added proxy for back-end communications and wired up basic communications example.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShowWeatherComponent } from './show-weather/show-weather/show-weather.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'weather', component: ShowWeatherComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowWeatherComponent } from './show-weather/show-weather/show-weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowWeatherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
